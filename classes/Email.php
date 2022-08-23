47d763ef6e4663fb4db09664a0e5d484ed897682
3duu21 <e5470843@gmail.com>
2022-08-22 22:25:57 -0400

    Deployment del proyecto

---
+++ classes/Email.php
@@ -0,0 +1,182 @@
+<?php 
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email {
+    public $email;
+    public $nombre;
+    public $token;
+
+    public function __construct($email, $nombre, $token){
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion(){
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'af7ac867cc6158';
+        $mail->Password = '2491c0b6b70d66';
+
+        $mail->setFrom('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->Subject = 'Confirma tu cuenta';
+
+        //Set html
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $mail->Body = "
+        <html>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
+        h2 {
+            font-size: 25px;
+            font-weight: 500;
+            line-height: 25px;
+        }
+    
+        body {
+            font-family: 'Poppins', sans-serif;
+            background-color: #ffffff;
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+    
+        p {
+            line-height: 18px;
+        }
+    
+        a {
+            position: relative;
+            z-index: 0;
+            display: inline-block;
+            margin: 20px 0;
+        }
+    
+        a button {
+            padding: 0.7em 2em;
+            font-size: 16px !important;
+            font-weight: 500;
+            background: #000000;
+            color: #ffffff;
+            border: none;
+            text-transform: uppercase;
+            cursor: pointer;
+        }
+        p span {
+            font-size: 12px;
+        }
+        div p{
+            border-bottom: 1px solid #000000;
+            border-top: none;
+            margin-top: 40px;
+        }
+    </style>
+    <body>
+        <h1>AppSalon</h1>
+        <h2>¡Gracias por registrarte ". $this->nombre. "!</h2>
+        <p>Por favor confirma tu correo electrónico para que puedas comenzar a disfrutar de todos los servicios de
+            BarberShop</p>
+        <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'><button>Verificar</button></a>
+        <p>Si tú no te registraste en BarberShop, por favor ignora este correo electrónico.</p>
+        <div><p></p></div>
+        <p><span>Este correo electrónico fue enviado desde una dirección solamente de notificaciones que no puede aceptar correo electrónico entrante. Por favor no respondas a este mensaje.</span></p>
+    </body>
+    </html>";
+
+        //Enviar el mail
+        $mail->send();
+    }
+
+    public function enviarIntrucciones(){
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'af7ac867cc6158';
+        $mail->Password = '2491c0b6b70d66';
+
+        $mail->setFrom('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->Subject = 'Reestablece tu Password';
+
+        //Set html
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $mail->Body = "
+        <html>
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
+        h2 {
+            font-size: 25px;
+            font-weight: 500;
+            line-height: 25px;
+        }
+    
+        body {
+            font-family: 'Poppins', sans-serif;
+            background-color: #ffffff;
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+    
+        p {
+            line-height: 18px;
+        }
+    
+        a {
+            position: relative;
+            z-index: 0;
+            display: inline-block;
+            margin: 20px 0;
+        }
+    
+        a button {
+            padding: 0.7em 2em;
+            font-size: 16px !important;
+            font-weight: 500;
+            background: #000000;
+            color: #ffffff;
+            border: none;
+            text-transform: uppercase;
+            cursor: pointer;
+        }
+        p span {
+            font-size: 12px;
+        }
+        div p{
+            border-bottom: 1px solid #000000;
+            border-top: none;
+            margin-top: 40px;
+        }
+    </style>
+    <body>
+        <h1>AppSalon</h1>
+        <h2>¡Hola ". $this->nombre. "!</h2>
+        <p>Has solisitado Reestablecer tu Password, sigue el siguiente enlace para hacerlo.</p>
+        <a href='http://localhost:3000/recuperar?token=" . $this->token . "'><button>Reestablecer Password</button></a>
+        <p>Si tú no solicitaste este cambio, por favor ignora este correo electrónico.</p>
+        <div><p></p></div>
+        <p><span>Este correo electrónico fue enviado desde una dirección solamente de notificaciones que no puede aceptar correo electrónico entrante. Por favor no respondas a este mensaje.</span></p>
+    </body>
+    </html>";
+
+        //Enviar el mail
+        $mail->send();
+    }
+}
